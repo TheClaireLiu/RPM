e9f5dd6c403c3e64aa5507646e8972a0d6d692d7
TheClaireLiu <cliu202211@gmail.com>
2024-09-01 22:49:42 -0400

    [UPDATE] update

--- components/dashboard/RentCards.tsx
+++ components/dashboard/RentCards.tsx
@@ -10,7 +10,7 @@ import useAppStore from "@/stores/appStore";
 interface RentCardsProps {
   propertyId?: string;
 }
-
+// test
 export default function RentCards({ propertyId }: RentCardsProps) {
 
   const {t} = useAppStore();
